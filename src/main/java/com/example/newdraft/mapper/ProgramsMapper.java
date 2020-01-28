0c0e9b2558a4010e162527aa750ef916e5e84c49
hutingting009 <59519062+hutingting009@users.noreply.github.com>
2020-01-28 18:47:44 +0800

    胡婷婷的第一次提交

--- src/main/java/com/example/newdraft/mapper/ProgramsMapper.java
+++ src/main/java/com/example/newdraft/mapper/ProgramsMapper.java
@@ -1,8 +1,12 @@
 package com.example.newdraft.mapper;
 
 import com.example.newdraft.model.pojo.Programs;
+import com.example.newdraft.model.vo.ProgramsVo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface ProgramsMapper {
@@ -12,4 +16,24 @@ public interface ProgramsMapper {
 
     Programs getProgramsById(@Param("id") String id);//通过编号获取项目信息
 
+    /**
+     *项目列表
+     * @param map 查询条件（项目名称模糊查询、国家名）
+     * @return
+     */
+    List<Programs> searchPrograms(Map<String, Object> map);
+
+    /**
+     * 项目详情页
+     * @param programId 项目ID
+     * @return 项目详情
+     */
+    ProgramsVo searchProgramInfo(@Param("programId") String programId);
+
+    /**
+     * 下架项目
+     * @param programId 项目ID
+     * @return 执行结果
+     */
+    int deleteProgramById(@Param("programId") String programId);
 }
